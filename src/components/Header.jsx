b3dc10ddaf66ff0752bbd62eca54fd7a5cf92d24
AlexeyTsukanov <alexeuel@gmail.com>
2020-03-16 22:57:26 +0200

    Ferst Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header className="header">
+      <img src='https://mirtankov.su/sites/default/files/img/company-of-heroes-2_0.jpg'></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return(
+    <nav className="navigation">
+        <div className="navigation_item">Home</div>
+        <div className="navigation_item">Forum</div>
+        <div className="navigation_item">Promo</div>
+        <div className="navigation_item">Contacts</div>
+      </nav>
+  );
+};
+
+export default Navbar;
